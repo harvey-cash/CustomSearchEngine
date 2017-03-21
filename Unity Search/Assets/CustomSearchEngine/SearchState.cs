84079987f11861226a9d8ffb702970f550596af7
HOCgaming <harveycash@outlook.com>
2017-03-21 11:26:51 +0000

    Abstract C# Classes

---
+++ Unity Search/Assets/CustomSearchEngine/SearchState.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+
+/* Abstract SearchState represents any one possible state / situation of the matter at hand.
+ * To use, the following variables, accessors, and methods must be implemented.
+ */
+
+public abstract class SearchState {
+
+    /* ~~~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~~~ */
+    /* ~~~~~~ (Include accessors of these!) ~~~~~~ */
+
+    protected int localCost;
+    public int GetLocalCost() { return localCost; }
+    public void SetLocalCost(int myLocalCost) { localCost = myLocalCost; }
+
+    protected int estRemainingCost;
+    public int GetEstRemainingCost() { return estRemainingCost; }
+    public void SetEstRemainingCost(int myEstRemainingCost) { estRemainingCost = myEstRemainingCost; }
+
+
+    /* ~~~~~~~~~~~~~ METHODS THAT MUST BE IMPLEMENTED ~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~ Remember to also include a .ToString() ! ~~~~~~~~~~ */
+
+    public abstract bool ReachedGoal(Search searcher); //Have you reached the goal?
+    public abstract bool SameState(SearchState compareState); //Is your state the same as this given one?
+
+    //Return an ArrayList of all the possible successor states
+    public abstract List<SearchState> GetSuccessors(Search searcher);
+}
